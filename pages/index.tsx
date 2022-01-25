30e297fbb18c72a74b282bd07aa05e596f431637
Caio Oliveira <52549124+caioliveira277@users.noreply.github.com>
2022-01-24 22:46:15 -0300

    atualizando url

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = 'https://nextjs-quiz-cod3r.vercel.app/'
 
 export default function Home() {
   const router = useRouter()
